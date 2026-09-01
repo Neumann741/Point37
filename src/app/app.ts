ec3bb76f4edb92ed962dbc42fb951bab8352c0e5
Emerson Luiz <emerson19812010@hotmail.com>
2026-09-01 17:03:10 -0300

    falta-logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./site/header/header";
 import { MainPage } from './site/main-page/main-page';
+import { Footer } from "./site/footer/footer";
 
 @Component({
-  imports: [RouterOutlet, Header, MainPage],
+  imports: [RouterOutlet, Header, MainPage, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
