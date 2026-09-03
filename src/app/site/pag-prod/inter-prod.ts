62fde6c1da808ccff1ac15529597e4c09a872562
Pablo Alexandre Lemos <alepablo797@gmail.com>
2026-09-03 16:27:54 -0300

    atualizar pagina de produtos

--- src/app/site/pag-prod/inter-prod.ts
+++ src/app/site/pag-prod/inter-prod.ts
@@ -1,7 +1,9 @@
 export interface InterProd {
-    nome: string,
-    categoria: string,
-    volume: number|string,
-    teor: number,
-    preço: number,
+    nome: string;
+    categoria: string;
+    volume: number|string;
+    teor: number|string;
+    preco: number;
+    
+   
 }
