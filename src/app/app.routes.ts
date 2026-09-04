fa3a4930d3d795a5f6b8b08da835b1856abe63f9
Pablo Alexandre Lemos <alepablo797@gmail.com>
2026-09-04 15:35:59 -0300

    separar carrossel do topbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { PagAdm } from './site/pag-adm/pag-adm';
 import { MainPage } from './site/main-page/main-page';
+import { PagProd } from './site/pag-prod/pag-prod';
 
 export const routes: Routes = [
 
 { path: 'main-page', component: MainPage},
+{path: 'pag-prod', component: PagProd},
 { path: 'pag-adm', component: PagAdm},
 { path: '', redirectTo: 'main-page' , pathMatch: 'full'}
 
