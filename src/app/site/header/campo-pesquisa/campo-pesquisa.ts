1e42ce530be3094b24f00a8e180155595dd8308e
Pablo Alexandre Lemos <alepablo797@gmail.com>
2026-09-08 16:33:36 -0300

    atualizar css

---
+++ src/app/site/header/campo-pesquisa/campo-pesquisa.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-campo-pesquisa',
+  styleUrl: './campo-pesquisa.css',
+  templateUrl: './campo-pesquisa.html',
+})
+export class CampoPesquisa {
+
+  
+}
