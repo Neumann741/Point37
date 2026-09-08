8e42b5a175a2589ad2830d1ed4be86dcfd302a5f
Emerson Luiz <emerson19812010@hotmail.com>
2026-09-08 16:13:25 -0300

    atualizacao carrinhho
    add + e - correto

--- src/app/site/header/carrinho/carrinho.ts
+++ src/app/site/header/carrinho/carrinho.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   imports: [],
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Output, output } from '@angular/core';
   templateUrl: './carrinho.html',
 })
 export class Carrinho {
-
   @Output() fechar = new EventEmitter<void>();
 
   produtos = [
@@ -16,14 +15,35 @@ export class Carrinho {
       nome: 'Produto Exemplo',
       preco: 29.90,
       quantidade: 1,
-      imagem: 'https://via.place.holder.cpm/80'
+      imagem: 'assets/tanqueray-transparente.png'
     },
     {
       nome: 'Outro produto',
       preco: 49.90,
       quantidade: 2,
-      imagem: 'https://via.placeholder.com/80'
+      imagem: 'assets/bombay-transparente.png'
 
     }
   ];
+
+  get subtotal(): number {
+    return this.produtos.reduce(
+      (total, produto) => total + produto.preco * produto.quantidade,
+      0,
+    );
+  }
+
+  diminuirQuantidade(index: number): void {
+    if (this.produtos[index].quantidade > 1) {
+      this.produtos[index].quantidade--;
+    }
+  }
+
+  aumentarQuantidade(index: number): void {
+    this.produtos[index].quantidade++;
+  }
+
+  removerProduto(index: number): void {
+    this.produtos.splice(index, 1);
+  }
 }
