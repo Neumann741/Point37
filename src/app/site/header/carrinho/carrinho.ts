493fad5cabb8cf681f8146e7fe7094ec853617c9
Emerson Luiz <emerson19812010@hotmail.com>
2026-09-08 16:03:15 -0300

    atualizacao carrinho
    aumentar e diminuir produtos

--- src/app/site/header/carrinho/carrinho.ts
+++ src/app/site/header/carrinho/carrinho.ts
@@ -1,11 +1,29 @@
-import { Component, output } from '@angular/core';
+import { Component, EventEmitter, Output, output } from '@angular/core';
 
 @Component({
   imports: [],
   selector: 'app-carrinho',
+  standalone: true ,
   styleUrl: './carrinho.css',
   templateUrl: './carrinho.html',
 })
 export class Carrinho {
-  fechar = output<void>();
+
+  @Output() fechar = new EventEmitter<void>();
+
+  produtos = [
+    {
+      nome: 'Produto Exemplo',
+      preco: 29.90,
+      quantidade: 1,
+      imagem: 'https://via.place.holder.cpm/80'
+    },
+    {
+      nome: 'Outro produto',
+      preco: 49.90,
+      quantidade: 2,
+      imagem: 'https://via.placeholder.com/80'
+
+    }
+  ];
 }
