1489d018c9320b26dbefe2a1adc9919a81b8b444
Arthur Neumann <arthurneumann18@gmail.com>
2026-09-09 14:34:09 -0300

    estiliza painel adm e outras funcionalidades (usei chat))

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { PagAdm } from './site/pag-adm/pag-adm';
 import { MainPage } from './site/main-page/main-page';
 import { PagProd } from './site/pag-prod/pag-prod';
+import { Login } from './site/login/login';
 
 export const routes: Routes = [
 
 { path: 'main-page', component: MainPage},
 {path: 'pag-prod', component: PagProd},
 { path: 'pag-adm', component: PagAdm},
+{ path: 'login', component: Login},
 { path: '', redirectTo: 'main-page' , pathMatch: 'full'}
 
 
