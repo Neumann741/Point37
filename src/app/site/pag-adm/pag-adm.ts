c0044980a5f34e7f909409bf95140b9d6ada3ea2
Arthur Neumann <arthurneumann18@gmail.com>
2026-09-13 20:58:59 -0300

    cira o put  request

--- src/app/site/pag-adm/pag-adm.ts
+++ src/app/site/pag-adm/pag-adm.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PostProduto } from "./post-produto/post-produto";
 import { DeleteProduto } from "./delete-produto/delete-produto";
+import { PutProduto } from "./put-produto/put-produto";
 
 @Component({
-  imports: [PostProduto, DeleteProduto],
+  imports: [PostProduto, DeleteProduto, PutProduto],
   selector: 'app-pag-adm',
   styleUrl: './pag-adm.css',
   templateUrl: './pag-adm.html',
