c8364cbb4e00dec0dc75bc836c80b60cf61f641b
Arthur Neumann <arthurneumann18@gmail.com>
2026-09-14 16:29:25 -0300

    add nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ export const routes: Routes = [
     { path: 'login', component: Login, },
    { path: 'carrinho', component: Carrinho },
   { path: 'finalizar-compra', component: FinalizarCompra },
+  { path: 'sobre', component: Sobre}
 
 
 
